55200391a3c6315e1456b75f2c334888664be5a0
xyy <xyy94813@sina.com>
2019-04-29 16:02:53 +0800

    chore:  upgrade libs
    upgrade `react-scripts` and  `react-app-rewired` to latest.
    (`react-app-rewired` v2 relay on `customize-cra`).
    we can remove the dependecy `react-app-rewire-less`.
    And we need to add `less` and `less-loader` to devDependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,36 +1,33 @@
-const { injectBabelPlugin } = require("react-app-rewired");
-const rewireLess = require("react-app-rewire-less");
+const {
+  override,
+  addBabelPlugins,
+  fixBabelImports,
+  addLessLoader
+} = require("customize-cra");
 
-module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.mjs$/,
-    include: /node_modules\/react-relay-network-modern/,
-    type: "javascript/auto"
-  });
+const babelPlugins = addBabelPlugins("relay", [
+  "module-resolver",
+  {
+    alias: {
+      "@": "./src"
+    }
+  }
+]);
 
-  config = injectBabelPlugin("relay", config);
-  config = injectBabelPlugin(
-    [
-      "module-resolver",
-      {
-        alias: {
-          "@": "./src"
-        }
-      }
-    ],
-    config
-  );
-  config = injectBabelPlugin(
-    [
-      "import",
-      {
-        libraryName: "antd",
+module.exports = {
+  webpack: (config, env) =>
+    override(
+      ...babelPlugins,
+      fixBabelImports("antd", {
         style: true
-      }
-    ],
-    config
-  );
-  config = rewireLess(config, env);
-  //do stuff with the webpack config...
-  return config;
+      }),
+      fixBabelImports("lodash", {
+        libraryDirectory: "",
+        camel2DashComponentName: false
+      }),
+      addLessLoader({
+        javascriptEnabled: true
+      })
+    )(config, env)
+  // jest: (config) => config,
 };
