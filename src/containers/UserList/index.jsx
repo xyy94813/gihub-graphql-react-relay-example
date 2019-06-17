b815d00ddb917a8d2544725f975f22a50c5052b7
xyy <xyy94813@sina.com>
2019-06-17 15:16:07 +0800

    refactor: Use `Statistic` to show user count

--- src/containers/UserList/index.jsx
+++ src/containers/UserList/index.jsx
@@ -1,145 +1,133 @@
-import React from 'react'
-import _ from 'lodash'
-import {
-  createPaginationContainer, 
-  graphql,
-  QueryRenderer
-} from 'react-relay'
-import {
-  Button,
-  Spin,
-  List,
-} from 'antd'
+import React from "react";
+import _ from "lodash";
+import { createPaginationContainer, graphql, QueryRenderer } from "react-relay";
+import { Button, Spin, List, Statistic } from "antd";
 
 import modernEnvironment from "../../createRelayEnvironment";
-import UserListItem from './UserListItem'
-import './UserList.css'
+import UserListItem from "./UserListItem";
+import "./UserList.css";
 
-const DEFAULT_PAGE_SIZE = 15
+const DEFAULT_PAGE_SIZE = 15;
 
 class UserList extends React.Component {
-  
   static defaultProps = {
     pageSize: DEFAULT_PAGE_SIZE
-  }
+  };
 
   state = {
     loadMore: false,
-    loading: false,
-  }
+    loading: false
+  };
 
   _loadMore = () => {
     if (this.state.loadMore) {
-      return
+      return;
     }
-    this.setState({
-      loadMore: true
-    }, () => {
-      this.props.relay.loadMore(DEFAULT_PAGE_SIZE, (error) => {
-        this.setState({ loadMore: false })
-        error && console.error(error)
-      })
-    })
-  }
+    this.setState(
+      {
+        loadMore: true
+      },
+      () => {
+        this.props.relay.loadMore(DEFAULT_PAGE_SIZE, error => {
+          this.setState({ loadMore: false });
+          error && console.error(error);
+        });
+      }
+    );
+  };
 
-  showMoreButton () {
+  showMoreButton() {
     if (this.props.relay.hasMore()) {
       return (
-        <Button 
-          onClick={this._loadMore}
-          className="UserList-loadMore-button"
-        >
+        <Button onClick={this._loadMore} className="UserList-loadMore-button">
           Load More
         </Button>
-      )
+      );
     }
   }
 
-  showFooterContent () {
+  showFooterContent() {
     return (
       <div className="UserList-footer">
-        { (this.state.loadMore) ? <Spin /> : this.showMoreButton() }
+        {this.state.loadMore ? <Spin /> : this.showMoreButton()}
       </div>
-    )
+    );
   }
 
-  render () {
-
-    const {
-      data
-    } = this.props
-    const userCount = _.get(data, 'users.userCount', 0)
-    const users = _.get(data, 'users.edges', []).map((item) => item.node)
+  render() {
+    const { data } = this.props;
+    const userCount = _.get(data, "users.userCount", 0);
+    const users = _.get(data, "users.edges", []).map(item => item.node);
     return (
       <div>
         <div className="UserList-header">
-          <span>
-            User Count: {userCount}
-          </span>
+          <Statistic title="User Count" value={userCount} />
         </div>
-        <List 
+        <List
           dataSource={users}
-          renderItem={(item) => (<UserListItem user={item}/>)}
+          renderItem={item => <UserListItem user={item} />}
           locale={{
-            emptyText: 'No Users. Please input other keyworld!!'
+            emptyText: "No Users. Please input other keyworld!!"
           }}
           loadMore={this.showFooterContent()}
         />
       </div>
-    )
-
+    );
   }
-
 }
 
 const QUERY = graphql`
-  query UserListQuery ($count: Int, $cursor: String, $query: String!) {
+  query UserListQuery($count: Int, $cursor: String, $query: String!) {
     ...UserList
   }
-`
+`;
 
-const UserListContainer = createPaginationContainer(UserList, graphql`
-  fragment UserList on Query  {
-    users: search(first: $count, after: $cursor, query: $query, type: USER)
-    @connection(key: "UserList_users") {
-      edges {
-        cursor,
-        node {
-          ...on User {
-            id
+const UserListContainer = createPaginationContainer(
+  UserList,
+  graphql`
+    fragment UserList on Query {
+      users: search(first: $count, after: $cursor, query: $query, type: USER)
+        @connection(key: "UserList_users") {
+        edges {
+          cursor
+          node {
+            ... on User {
+              id
+            }
+            ...UserListItem_user
           }
-          ...UserListItem_user
+        }
+        userCount
+        pageInfo {
+          startCursor
+          endCursor
+          hasNextPage
+          hasPreviousPage
         }
       }
-      userCount
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
     }
+  `,
+  {
+    direction: "forward",
+    getConnectionFromProps(props) {
+      return _.get(props, "data.users", null);
+    },
+    getFragmentVariables(prevVars, totalCount) {
+      return {
+        ...prevVars,
+        count: totalCount
+      };
+    },
+    getVariables(props, prevVars, fragmentVariables) {
+      return {
+        ...fragmentVariables,
+        ...prevVars,
+        ...props
+      };
+    },
+    query: QUERY
   }
-`, {
-  direction: 'forward',
-  getConnectionFromProps(props) {
-    return _.get(props, 'data.users', null)
-  },
-  getFragmentVariables(prevVars, totalCount) {
-    return {
-      ...prevVars,
-      count: totalCount,
-    };
-  },
-  getVariables(props, prevVars, fragmentVariables) {
-    return {
-      ...fragmentVariables,
-      ...prevVars,
-      ...props,
-    };
-  },
-  query: QUERY
-})
+);
 
 function renderDetails({ error, props }) {
   if (error) {
@@ -147,18 +135,22 @@ function renderDetails({ error, props }) {
   } else if (props) {
     return <UserListContainer data={props} />;
   } else {
-    return <Spin><div style={{ minHeight: 300 }}/></Spin>
+    return (
+      <Spin>
+        <div style={{ minHeight: 300 }} />
+      </Spin>
+    );
   }
 }
 
-export default (props) => (
+export default props => (
   <QueryRenderer
     environment={modernEnvironment}
     query={QUERY}
     variables={{
-      query: props.searchText || '',
+      query: props.searchText || "",
       count: DEFAULT_PAGE_SIZE
     }}
     render={renderDetails}
   />
-)
+);
